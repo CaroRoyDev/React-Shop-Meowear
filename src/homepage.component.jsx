fe7a35f294ce6979428d777d2c193995ccedec02
Caroline Roy <caroline.roy.tln@gmail.com>
2021-08-04 19:29:52 +0200

    Init project and basic homepage component

---
+++ src/homepage.component.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import './homepage.styles.scss'
+const HomePage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1>Jackets</h1>
+                    <span>Shop now</span>
+                </div>
+            </div>
+
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1>Jackets</h1>
+                    <span>Shop now</span>
+                </div>
+            </div>
+
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1>Jackets</h1>
+                    <span>Shop now</span>
+                </div>
+            </div>
+
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1>Jackets</h1>
+                    <span>Shop now</span>
+                </div>
+            </div>
+
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1>Jackets</h1>
+                    <span>Shop now</span>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+);
+
+export default HomePage;
