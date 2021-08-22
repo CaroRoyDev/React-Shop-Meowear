d7076db234ec008c527ffdaf596851cd0f69e0db
Caroline Roy <caroline.roy.tln@gmail.com>
2021-08-22 11:23:52 +0200

    Updated redirection on user auth

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import { setCurrentUser } from "./redux/user/user.actions";
 import "./App.css";
@@ -8,7 +8,7 @@ import Header from "./components/header/header.component";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
-import Authentication from "./pages/authentication/authentication.component";
+import AuthenticationPage from "./pages/authentication/authentication.component";
 
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
@@ -38,21 +38,32 @@ class App extends React.Component {
   }
 
   render() {
+    const { currentUser } = this.props;
     return (
       <div>
         <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/shop" component={ShopPage} />
-          <Route exact path="/signin" component={Authentication} />
+          <Route
+            exact path="/signin"
+            render={() => 
+              currentUser ? <Redirect to="/" /> : <AuthenticationPage />
+            }
+          />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser,
+});
+
 const mapDispatchToProps = (dispatch) => ({
+  
   setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/pages/authentication/authentication.component.jsx
+++ src/pages/authentication/authentication.component.jsx
@@ -4,11 +4,11 @@ import "./authentication.styles.scss";
 import SignIn from "../../components/sign-in/sign-in.component";
 import SignUp from "../../components/sign-up/sign-up.component";
 
-const Authentication = () => (
+const AuthenticationPage = () => (
     <div className="authentication">
         <SignIn/>
         <SignUp/>
     </div>
 )
 
-export default Authentication;
+export default AuthenticationPage;
